ec91e68920a523aedaddc9268257e5ff6429a936
Eric RANDRIANARISOA <raneric@gmail.com>
2024-12-11 21:29:26 +0300

    Day01

--- src/Day01/Day01.kt
+++ src/Day01/Day01.kt
@@ -2,21 +2,46 @@ package Day01
 
 import Utils.println
 import Utils.readInput
+import kotlin.math.abs
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    val testInput = readInput("Day01/Day01_test")
+    val input = readInput("Day01/Day01")
+
+    val left = input.getLeft()
+    val right = input.getRight()
+
+    fun part1(): Int {
+        val sortedLeft = left.sorted()
+        val sortedRight = right.sorted()
+        var sum = 0
+        sortedLeft.forEachIndexed{ index, value->
+            sum += abs(value - sortedRight[index])
+        }
+        return sum
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(): Int {
+        return left.fold(0) { acc, i ->
+            acc + ( i * right.count { it == i })
+        }
     }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01/Day01_test")
 
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01/Day01")
-    part1(input).println()
-    part2(input).println()
+    part1().println()
+    part2().println()
+}
+
+private fun List<String>.getLeft(): List<Int> {
+    return this.map {
+        it.split(" ")[0].toInt()
+    }
+}
+
+private fun List<String>.getRight(): List<Int> {
+    return this.map {
+        it.split(" ").filter { value -> value != "" }[1].toInt()
+    }
 }
