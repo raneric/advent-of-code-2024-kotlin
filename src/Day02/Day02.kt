f7efd533494fc9815fc6c4679d39014cff43efcb
Eric RANDRIANARISOA <raneric@gmail.com>
2024-12-14 13:58:46 +0300

    test

---
+++ src/Day02/Day02.kt
@@ -0,0 +1,22 @@
+package Day02
+
+import Utils.println
+import Utils.readInput
+
+fun main() {
+
+    val testInput = readInput("Day02/Day02_test")
+    val input = readInput("Day02/Day02")
+
+    fun part1(): Int {
+
+        return 0
+    }
+
+    fun part2(): Int {
+        return 0
+    }
+
+    part1().println()
+    part2().println()
+}
